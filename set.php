742d4c2c52174e917fa79da78bac837276b657d2
Cristche Paredes <cristcheparedes@gmail.com>
2017-08-10 16:16:00 -0400

    make it better

--- set.php
+++ set.php
@@ -4,7 +4,7 @@ require __DIR__ . '/vendor/autoload.php';
 
 $bot_api_key  = '410216312:AAHOrXDU7x7knIhyhFy2AnE_s5UN_HW9J74';
 $bot_username = 'FCCWBot';
-$hook_url = 'https://https://telegbot517.herokuapp.com/hook.php';
+$hook_url = 'https://telegbot517.herokuapp.com/hook.php';
 
 try {
     // Create Telegram API object
